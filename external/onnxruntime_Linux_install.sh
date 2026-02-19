92426727eb714ea101682316dc28cd0e57ce85dc
Remdox <marco11maxing@gmail.com>
2026-02-19 17:34:14 +0100

    Added robustness to onnxruntime installation script

--- external/onnxruntime_Linux_install.sh
+++ external/onnxruntime_Linux_install.sh
@@ -1,9 +1,13 @@
 tar -zxvf onnxruntime-linux-x64-1.21.0.tgz
 sudo cp -r onnxruntime-linux-x64-1.21.0/lib/libonnxruntime* /usr/local/lib64/.
 
-sudo mkdir /usr/local/lib64/cmake/
+sudo mkdir -p /usr/local/lib64/cmake/
 sudo cp -r onnxruntime-linux-x64-1.21.0/lib/cmake/onnxruntime/ /usr/local/lib64/cmake/.
 
-sudo cp -r onnxruntime-linux-x64-1.21.0/include/onnxruntime/ /usr/local/include/.
+if [ -d "onnxruntime-linux-x64-1.21.0/include/onnxruntime/" ]; then
+    sudo cp -r onnxruntime-linux-x64-1.21.0/include/onnxruntime/ /usr/local/include/.
+else
+    sudo cp -r onnxruntime-linux-x64-1.21.0/include/* /usr/local/include/.
+fi
 
 sudo ldconfig
--- external/onnxruntime_Linux_uninstall.sh
+++ external/onnxruntime_Linux_uninstall.sh
@@ -2,4 +2,20 @@ sudo rm /usr/local/lib64/libonnxruntime*
 
 sudo rm -r /usr/local/lib64/cmake/onnxruntime
 
-sudo rm -r /usr/local/include/onnxruntime
+if [ -d "/usr/local/include/onnxruntime" ]; then
+    sudo rm -rf /usr/local/include/onnxruntime
+else
+    sudo rm -f /usr/local/include/cpu_provider_factory.h \
+               /usr/local/include/onnxruntime_c_api.h \
+               /usr/local/include/onnxruntime_cxx_api.h \
+               /usr/local/include/onnxruntime_cxx_inline.h \
+               /usr/local/include/onnxruntime_float16.h \
+               /usr/local/include/onnxruntime_lite_custom_op.h \
+               /usr/local/include/onnxruntime_run_options_config_keys.h \
+               /usr/local/include/onnxruntime_session_options_config_keys.h \
+               /usr/local/include/provider_options.h
+
+    if [ -f "/usr/local/include/core/providers/resource.h" ]; then
+        sudo rm -rf /usr/local/include/core/
+    fi
+fi
